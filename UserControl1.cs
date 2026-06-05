6ecbad25c668661f434e18cad29016f33a8eebaa
nickglazunov <nickglazunov1@gmail.com>
2026-06-05 17:33:16 +0700

    Update UserControl1.cs

--- UserControl1.cs
+++ UserControl1.cs
@@ -1,19 +1,21 @@
+using System;
 using System.Windows.Forms;
-
-namespace WindowsFormsApp8
+namespace KursGlazunov
 {
     public partial class UserControl1 : UserControl
     {
+        public void Card(string fullNameStudents, string nameDiscipline, string fullNameTeacher, string status, string mark, string description)
+        {
+            label1.Text = fullNameStudents;
+            label3.Text = nameDiscipline;
+            label5.Text = fullNameTeacher;
+            label7.Text = status;
+            label9.Text = mark;
+            label11.Text = description;
+        }
         public UserControl1()
         {
             InitializeComponent();
-        }
-
-        public void Card(string product_name, string description, decimal price)
-        {
-            label1.Text = product_name;
-            label2.Text = description;
-            label3.Text = $"{price.ToString()} руб.";
         }
     }
 }
