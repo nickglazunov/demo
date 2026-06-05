859cebf778f0c42f706eeeb658cdcd25fedd5f09
nickglazunov <nickglazunov1@gmail.com>
2026-06-05 17:32:05 +0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,20 @@
 using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
-
-namespace WindowsFormsApp1
+namespace KursGlazunov
 {
     public partial class Form1 : Form
     {
-        public string connString = "Server=HONELUVSWAGGA\\SQLEXPRESS;Database=Demo1;Integrated Security=True;TrustServerCertificate=True";
+        public string connStr = "Server=localhost;Database=Stydent;Integrated Security=True;TrustServerCertificate=True";
         public Form1()
         {
             InitializeComponent();
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Вы вошли как гость", "Добро пожаловать", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            Form2 newForm = new Form2();
-            newForm.Show();
+            MessageBox.Show("Вы вошли как гость!", "Добро пожаловать", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Form2 newform = new Form2();
+            newform.Show();
             this.Hide();
         }
         private void button1_Click(object sender, EventArgs e)
@@ -25,26 +23,26 @@ namespace WindowsFormsApp1
             string password = textBox2.Text;
             if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
             {
-                MessageBox.Show("Заполните поля", "Поля не могут быть пустыми", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Заполните оба поля", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
-            SqlConnection sqlconn = new SqlConnection(connString);
-            sqlconn.Open();
-            string query = "SELECT * FROM [User] WHERE login=@login AND password=@password";
-            SqlCommand sqlcomm = new SqlCommand(query, sqlconn);
-            sqlcomm.Parameters.AddWithValue("@login", login);
-            sqlcomm.Parameters.AddWithValue("@password", password);
-            SqlDataReader reader = sqlcomm.ExecuteReader();
-            if (reader.Read()) 
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            string query = "SELECT * FROM [Students] WHERE Email=@login AND idStudent=@password";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@login", login);
+            cmd.Parameters.AddWithValue("@password", password);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
             {
                 MessageBox.Show("Успешная авторизация", "Добро пожаловать", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                Form2 newForm = new Form2();
-                newForm.Show();
+                Form2 newform = new Form2();
+                newform.Show();
                 this.Hide();
             }
             else
             {
-                MessageBox.Show("Введен неверный логин или пароль", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
     }
